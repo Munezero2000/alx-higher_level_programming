e55c9525b1da25a94161ea3667036f0304e61db3
Munezero2000 <101561925+Munezero2000@users.noreply.github.com>
2022-07-02 21:25:22 +0200

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-   """Print the number of and list of arguments."""
+    """Print the number of and list of arguments."""
     import sys
-    """removed the name of the script at index 0"""
-    arg = len(sys.argv) - 1  
+
+    arg = len(sys.argv) - 1
     if arg == 0:
         print("0 arguments.")
     elif arg == 1:
         print("1 argument:")
     else:
         print("{} arguments:".format(arg))
-        """" printing args with their index """
     for i in range(arg):
-        print("{} : {}".format(i + 1, sys.argv[i + 1]))
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
