483d2e49ed243d0b970977145079193456920593
Munezero2000 <101561925+Munezero2000@users.noreply.github.com>
2022-07-03 12:44:52 +0200

    Update 5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""print value of a stored in variablw_5.py module."""
 if __name__ == "__main__":
-    a = 98
-    print(a)
+    import variable_load_5.py
+    val_a = variable_load_5.a
+    print(val_a)
